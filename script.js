7343e0a4407c9f03c4ffd069736f1a50ba3eb847
Lai Ze Min <zeminlai426@gmail.com>
2022-08-30 01:34:55 +0800

    Fix delete feature bugs, style the calculator and reorganise code

--- script.js
+++ script.js
@@ -25,24 +25,84 @@ let pastOperator = function (x, y){
     return x;
 };
 
-for (numBtn of numButtons) {
-    numBtn.addEventListener("click", (e) => {
-        if (initialNumpad && (num1String.length <= 15)){
-            num1String += e.target.textContent;
-            console.log(`num1string: ${num1String}`);
-            num1 = parseFloat(num1String);    
-            currentDisplay.textContent = num1String;
-        }
-    })
+// num1 numpad is active
+num1Input();
+
+divideBtn.addEventListener("click", () => {
+    process(divide,"/");
+});
+plusBtn.addEventListener("click", () => {
+    process(add,"+");
+});
+minusBtn.addEventListener("click", () => {
+    process(subtract, "-");
+});
+mulBtn.addEventListener("click", () => {
+    process(multiply, "x");
+});
+
+equalBtn.addEventListener("click", (e) => {
+    let result = operate(pastOperator, num1, num2);
+    
+    // If result have decimal places, fix it to 5 dp
+    if (result % 1 != 0) result = result.toFixed(5);
+    
+    // Display section
+    console.log(`result: ${result}`);
+    bgDisplay.textContent = `${resultBgDisplay} ${num2} =`;
+    currentDisplay.textContent = result;
+
+    // Set num2 and num2String to result for delete feature after equal
+    num2String = result.toString();
+    num2 = result;
+    pastOperator = equal;
+})
+
+clearBtn.addEventListener("click", (e) => {
+    initializeAll();
+})
+
+deleteBtn.addEventListener("click", () => {
+    console.log("delete");
+    if(initialNumpad){
+        // If num1 numpad is active, slice num1String
+        num1String = num1String.slice(0, -1);
+        num1 = parseFloat(num1String);
+        currentDisplay.textContent = num1String;
+    }
+    else {
+        // If num2 numpad is active, slice num2String
+        num2String = num2String.slice(0, -1);
+        num2 = parseFloat(num2String);
+        console.log(`num2: ${num2}`)
+        currentDisplay.textContent = num2String;
+    }
+})
+
+decimalBtn.addEventListener("click", decimal);
+
+function num1Input(){
+    for (numBtn of numButtons) {
+        numBtn.addEventListener("click", (e) => {
+            // If num1 numpad is active and string does not exceed limit
+            if (initialNumpad && (num1String.length <= 15)){
+                // Append number chosen to num1String
+                num1String += e.target.textContent;
+                console.log(`num1string: ${num1String}`);
+                num1 = parseFloat(num1String);    
+                currentDisplay.textContent = num1String;
+            }
+        })
+    }
 }
 
-
-function numpad(){
+function num2Input(){
     for (numBtn of numButtons) {
         numBtn.addEventListener("click", getNum2)
     }
 }
 function getNum2(e) {
+    // If num1 numpad is off
     if ((!initialNumpad) && (num2String.length <= 15)){
         num2String += e.target.textContent;
         num2 = parseFloat(num2String);
@@ -53,55 +113,44 @@ function getNum2(e) {
     }
 }
 
-divideBtn.addEventListener("click", (e) => {
-    process(divide,"/");
-});
-plusBtn.addEventListener("click", (e) => {
-    process(add,"+");
-});
-minusBtn.addEventListener("click", (e) => {
-    process(subtract, "-");
-});
-mulBtn.addEventListener("click", (e) => {
-    process(multiply, "x");
-});
 
-equalBtn.addEventListener("click", (e) => {
-    let result = operate(pastOperator, num1, num2);
-    if (result % 1 != 0) result = result.toFixed(5);
-    console.log(`result: ${result}`);
-    bgDisplay.textContent = resultBgDisplay + num2;
-    currentDisplay.textContent = result;
-})
-
-clearBtn.addEventListener("click", (e) => {
-    initializeAll();
-})
-
-deleteBtn.addEventListener("click", () => {
-    console.log("delete");
+function process(operator,sign) {
+    // If num1 numpad is on
     if(initialNumpad){
-        num1String = num1String.slice(0, -1);
-        num1 = parseFloat(num1String);
-        currentDisplay.textContent = num1String;
+        // Turn off num1 numpad if operator is pressed
+        initialNumpad = false;
+        // Store operator chosen to be used when another operator is clicked
+        pastOperator = operator;
+        console.log(operator);
+        
+        bgDisplay.textContent = `${num1} ${sign}`;
+        currentDisplay.textContent = `${num1} ${sign}`;
+        resultBgDisplay = num1String + " " +sign;
+        // Turn on num2 numpad
+        num2Input();
     }
-    else {
-        num2String = num2String.slice(0, -1);
-        num2 = parseFloat(num2String);
-        currentDisplay.textContent = num2String;
+    // If num2 numpad is on
+    else{
+        // If operation button was clicked more 2 times in a row, exit function
+        if (num2String === "") return;
+        console.log(operator);
+        console.log(`num1: ${num1}`);
+        console.log(`num2: ${num2}`);
+        
+        // Store answer in num1
+        num1 = operate(pastOperator, num1, num2);
+        bgDisplay.textContent = `${num1} ${sign}`;
+        resultBgDisplay = `${num1} ${sign}`;
+        pastOperator = operator;
+        
+        console.log(`num1: ${num1}`);
+        // Initialize num2 and num2String
+        num2String = "";
+        num2 = 0;
+        currentDisplay.textContent = num2;
     }
-})
+}
 
-decimalBtn.addEventListener("click", () => {
-    if (initialNumpad) {
-        num1String += ".";
-        currentDisplay.textContent = num1String;
-    }
-    else {
-        num2String += ".";
-        currentDisplay.textContent = num2String;
-    }
-})
 function add(x, y) {
     return x  + y;
 }
@@ -113,42 +162,29 @@ function subtract(x, y) {
 function multiply(x, y) {
     return x * y;
 }
-let divide = function (x, y) {
+function divide(x, y) {
     return x / y;
 }
-
 
 function operate(func, x, y) {
     return func(x,y);
 }
 
-function process(operator,sign) {
-    if(initialNumpad){
-        initialNumpad = false;
-        pastOperator = operator;
+// Just return num2(editable with deleteBtn) if equal was last pressed
+function equal(x, y){
+    return y;
+}
 
-        console.log(operator);
-
-        bgDisplay.textContent = num1String + sign;
-        currentDisplay.textContent = num1String + sign;
-        resultBgDisplay = num1String + sign;
-        numpad(num2String);
+function decimal(){
+    // If the string already contains a decimal, exit function
+    if (currentDisplay.textContent.includes(".")) return;
+    if (initialNumpad) {
+        num1String += ".";
+        currentDisplay.textContent = num1String;
     }
-    else{
-        console.log(operator);
-        console.log(`num1: ${num1}`);
-        console.log(`num2: ${num2}`);
-
-        num1 = operate(pastOperator, num1, num2);
-        bgDisplay.textContent = num1 + sign;
-        resultBgDisplay = num1 + sign;
-        pastOperator = operator;
-
-        console.log(`num1: ${num1}`);
-
-        num2String = "";
-        num2 = 0;
-        currentDisplay.textContent = num2;
+    else {
+        num2String += ".";
+        currentDisplay.textContent = num2String;
     }
 }
 
